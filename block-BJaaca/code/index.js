073e9c2e66a831b02814b142fef6eae00bad3dee
Shazia Khan <shaziya5416@gmail.com>
2021-11-14 10:52:58 +0530

    changes made

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -6,7 +6,9 @@ let character={};
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
-character.characterName="Arya"
+character{
+characterName="Arya"
+}
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 let age = 20;
 character.character-age=age;
@@ -16,15 +18,19 @@ character["sur name"]="Stark"
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
 character.title=`Lady of winterfell`
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
-character.greet=alert(`I am ${characterName} and my title is ${title}`)
+character.greet=function(){
+alert(`I am ${character.characterName} and my title is ${character.title}`);
+}
 // - Add a new property to the character object named `isFemale` and value will be `true`
 character.isFemale=true
 // - Execute the function stored in the greet key of the character object
 character.greet
 //  - Change the method `greet` to now alert `She is [NAME HERE] and her title is [TITLE HERE]`.
-character.greet=alert(`${greet}`)
+character.greet=function(){
+alert(`she is $(character.characterName) and the title is ${character.title}`);
+}
 // - Check by calling `greet` and see if the message changed or not.
-character.greet
+character.greet();
 //  - Add a new property in the character object `isAdult`. The value will be true if age is greater than 18 or it will be false.
 if (age>18){
 character.isAdult=true}
@@ -39,12 +45,13 @@ return character.isAdult;
 // - Add a new key with the name stored in variable `keyName` defined below i.e `playedBy` (use [] for computed property) and the value of "Maisie Williams"
 
 let keyName = 'playedBy';
-
+character["keyName"]=`Massie Williams`;
 
 // - Using `alert` alert the value stored in key you added above `keyName` use the variable name to access
-alert(
+alert(character(["keyName"])
 // - Using the function `console.log` log the value of the key `42`. You don't have to add the key
-
+console.log(character[42])
 // - Add a new key named `totalSeasons` with the value of `1 + 2 + 3 + 2`. Use this expression
-
+character.totalSeasons=1+2+3+2;
 // - Access the value of the key `totalSeason` (it should be 8)
+console.log(character.totalSeasons);
